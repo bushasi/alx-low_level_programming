104278afaf7d2761527e4792b4f060fc675d3be8
Asiphe Bushula <asiphebushula99@gmail.com>
2022-12-20 05:26:33 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -11,6 +12,7 @@
 int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fwrite(2, err, 59);
 
 	return (1);
