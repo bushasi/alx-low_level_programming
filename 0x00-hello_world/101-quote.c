7287f644c3d26025c30374bc9076c2300f1f6739
Asiphe Bushula <asiphebushula99@gmail.com>
2022-12-20 05:15:42 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1 (Error)
+ *
+*/
+
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(2, err, 59);
+	return (1);
+}
